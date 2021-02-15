0a661622f63810ccc465a412497fb5eafeb3458e
Guangwen <luguangwen2018@gmail.com>
2021-02-15 15:16:32 -0500

    Fix the template bug

--- backend/drf/urls.py
+++ backend/drf/urls.py
@@ -4,7 +4,7 @@ from rest_framework.documentation import include_docs_urls
 from drf import views
 
 urlpatterns = format_suffix_patterns([
-    url(r'^docs/', include_docs_urls(title="Texada Rest APIs")),
+    url(r'^docs/', include_docs_urls(title="PerkinElmer Rest APIs")),
     url(r'^product-delete/(?P<pk>[0-9]+)/$', views.DestroyProduct.as_view()),
     url(r'^product/(?P<pk>[0-9]+)/$', views.UpdateProduct.as_view()),
     url(r'^product/', views.ListCreateProduct.as_view()),
