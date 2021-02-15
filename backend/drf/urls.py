9985d2fd08820506cbfb39f51f0b5ec14dc413ef
Guangwen <luguangwen2018@gmail.com>
2021-02-15 13:28:09 -0500

    Change texada to drf

--- backend/drf/urls.py
+++ backend/drf/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.documentation import include_docs_urls
-from texada import views
+from drf import views
 
 urlpatterns = format_suffix_patterns([
     url(r'^docs/', include_docs_urls(title="Texada Rest APIs")),
